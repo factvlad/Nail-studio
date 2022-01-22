fc346fc6cd0c68aa096b5bb30465eebc6ad95924
Vlad <fact1@ukr.net>
2022-01-22 18:27:30 +0200

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,50 +1,31 @@
-$(function () {
-  $(".menu__btn").on("click", function () {
-    $(".menu__list").toggleClass("active");
-  });
+$(function() {
+    $(".menu__btn").on("click", function() {
+        $(".menu__list").toggleClass("active");
+    });
 
-  $(".works__slider").slick({
-    dots: true,
-    arrows: true,
-    fade: true,
-    autoplay: true,
-    prevArrow: "<img src='images/prewArrow.svg' class='prev' alt='prev'>",
-    nextArrow: "<img src='images/nextArrow.svg' class='next' alt='next'>",
-  });
+    $(".works__slider").slick({
+        dots: true,
+        arrows: true,
+        fade: true,
+        autoplay: true,
+        prevArrow: "<img src='images/prewArrow.svg' class='prev' alt='prev'>",
+        nextArrow: "<img src='images/nextArrow.svg' class='next' alt='next'>",
+    });
 
-  $('a[href^="#"]').on("click", function (event) {
-    event.preventDefault();
+    $('a[href^="#"]').on("click", function(event) {
+        event.preventDefault();
 
-    var sc = $(this).attr("href"),
-      dn = $(sc).offset().top;
-    $("html, body").animate({ scrollTop: dn }, 1000);
-  });
+        var sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
+        $("html, body").animate({ scrollTop: dn }, 1000);
+    });
 
-  // $(document).ready(function () {
-  //   var button = $("#button-up");
-  //   $(window).scroll(function () {
-  //     if ($(this).scrollTop() > 300) {
-  //       button.fadeIn();
-  //     } else {
-  //       button.fadeOut();
-  //     }
-  //   });
-  //   button.on("click", function () {
-  //     $("body, html").animate(
-  //       {
-  //         scrollTop: 0,
-  //       },
-  //       800
-  //     );
-  //     return false;
-  //   });
-  // });
 });
 
 let yWidgetSettings = {
-  buttonColor: "#D2B57D",
-  buttonPosition: "bottom right",
-  buttonAutoShow: true,
-  buttonText: "Онлайн запись",
-  formPosition: "right",
+    buttonColor: "#D2B57D",
+    buttonPosition: "bottom right",
+    buttonAutoShow: true,
+    buttonText: "Онлайн запись",
+    formPosition: "right",
 };
